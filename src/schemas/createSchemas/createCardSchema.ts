3555e33600148d3abbafc76918e6b30a49ae4c81
joaofelix27 <joaofelix27@gmail.com>
2022-09-11 14:18:47 -0300

    All requisits done

--- src/schemas/createSchemas/createCardSchema.ts
+++ src/schemas/createSchemas/createCardSchema.ts
@@ -1,12 +1,12 @@
 import joi from "joi"
 
-export const createCardSchema =joi.object ({
+export const createCardSchema =joi.object().keys ({
     number:joi.string().length(16).required(),
     name:joi.string().required(),
     securityCode:joi.string().length(3).required(),
     expirationDate:joi.string().length(5).required(),
     password:joi.string().required(),
     title:joi.string().required(),
-    isVirtual:joi.boolean().required(),
+    isVirtual:joi.boolean().strict().required(),
     type:joi.string().valid("Credit","Debit","Value").required()
 })
